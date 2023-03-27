176569520d0255ed427a88735d1371b57538963e
Roman Popov <i@romanpopoff.ru>
2023-03-27 12:05:20 +0300

    dynamik content

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ this.$route.params.slug }}</div>
+</template>
+
+<script>
+
+export default {
+  async asyncData ({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+    return { article }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/projects/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: '_slig.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
