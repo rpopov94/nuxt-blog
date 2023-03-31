83a312e1e56a1f8dd242e4454b5a61e3769641ca
Roman Popov <i@romanpopoff.ru>
2023-03-31 11:40:42 +0300

    Updates

--- _nuxt/static/1680251408/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680251408",layout:"default",error:a,state:{articles:{article:a,articles:[]},projects:{project:a,projects:[]},store:{}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"745646d0"}}}}(null,"\u002F"));
