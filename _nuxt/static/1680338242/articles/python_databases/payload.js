c98b276e57b78b906277f17e053752af0a8bab06
Roman Popov <i@romanpopoff.ru>
2023-04-01 11:37:37 +0300

    Updates

---
+++ _nuxt/static/1680338242/articles/python_databases/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/articles/python_databases", {data:[{}],fetch:{},mutations:[["articles\u002FsetArticle",[{layout:"Базы данных в Python",abstract:"Эта статья описывает работу с базами данных в Python",tags:["ORM","SQLAlchemy","Django ORM"],image:"Python-SQL.png",body:{type:"root",children:[]},createdAt:"2023-04-01T08:26:36.118Z",updatedAt:"2023-04-01T08:26:36.128Z"}]]]});
