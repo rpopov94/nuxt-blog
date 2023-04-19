a34c933893a0368b904d5617e468ec002dd8cf1f
rpopov94 <rpopov94@users.noreply.github.com>
2023-04-19 14:56:08 +0000

    deploy: bafafbf3b3830dd0572bd5d1be94c8fc4096c954

--- _nuxt/static/1680505918/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Farticles","\u002F","\u002Fcontacts","\u002F404","\u002Fprojects"]})
--- _nuxt/static/1680505918/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680505918",layout:"default",error:a,state:{articles:{article:a,articles:[]},projects:{project:a,projects:[]},store:{}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b50a41a2"}}}}(null,"\u002F"));
