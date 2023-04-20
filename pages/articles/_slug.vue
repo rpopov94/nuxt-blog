f26d6b086d98780d8286c045d64c0ebb387036de
Roman Popov <i@romanpopoff.ru>
2023-04-20 13:41:46 +0300

    Add GitHub Actions workflow

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -2,24 +2,24 @@
   <div class="mt-auto">
     <div class="container min-vh-100">
       <main class="pb-5">
-    <img
-      v-if="article.image"
-      :src="'images/' + article.image"
-      class="pt-2 w-full object-cover cover-image"
-      style="height: 24rem; width: 100%; padding-top: 0"
-      :alt="article.image"
-    />
-    <h1 class="prose text-4xl leading-9 py-4 font-bold text-center">
-      {{ article.layout }}
-    </h1>
-    <h6 class="prose leading-9 font-italic font-weight-light">
-      Описание: {{ article.abstract }}
-    </h6>
-    <Tags :tags="article.tags" />
-    <p class="blog-date text-gray-500 mb-4">
-      Последнее обновление: {{ formatDate(article.updatedAt) }}
-    </p>
-    <nuxt-content class="markdown" :document="article" />
+        <img
+          v-if="article.image"
+          :src="'images/' + article.image"
+          class="pt-2 w-full object-cover cover-image"
+          style="height: 24rem; width: 100%; padding-top: 0"
+          :alt="article.image"
+        >
+        <h1 class="prose text-4xl leading-9 py-4 font-bold text-center">
+          {{ article.layout }}
+        </h1>
+        <h6 class="prose leading-9 font-italic font-weight-light">
+          Описание: {{ article.abstract }}
+        </h6>
+        <Tags :tags="'' + article.tags" />
+        <p class="blog-date text-gray-500 mb-4">
+          Последнее обновление: {{ formatDate(article.updatedAt) }}
+        </p>
+        <nuxt-content class="markdown" :document="article" />
       </main>
     </div>
   </div>
