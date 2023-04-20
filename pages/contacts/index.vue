7d373c3225620db6fd9ba71805e972ed65b620ba
Roman Popov <i@romanpopoff.ru>
2023-04-20 14:31:31 +0300

    fix view for contacts

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="vh-100 mt-3">
+  <div class="vh-100 mt-5">
+    <div class="text-center"><h1>Contacts</h1></div>
     <div class="row">
       <div class="col-md-6 order-md-2">
         <div>
-          <div>
+          <div class="mt-2">
             <h5>Popov Roman</h5>
             <p><a href="tel:+79102723716"><img src="/icons/tel.svg" alt="phone"></a> +7(910) 272 37-16</p>
             <p><a href="mailto:i@romanpopoff.ru"><img src="/icons/mail.svg" alt="e-mail"></a> i@romanpopoff.ru</p>
