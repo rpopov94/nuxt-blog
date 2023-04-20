a78aab5d0c836394a1aa5a51b5913ba8c5c61e84
Roman Popov <i@romanpopoff.ru>
2023-04-20 14:10:13 +0300

    Add GitHub Actions workflow

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="text-center">
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="/nuxt_portfolio_blog/">
+      <b-navbar-brand href="/">
         Popov Roman
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse" />
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/nuxt_portfolio_blog/files/resume.pdf">
+          <b-nav-item href="/files/resume.pdf">
             Resume
           </b-nav-item>
-          <b-nav-item href="/nuxt_portfolio_blog/contacts">
+          <b-nav-item href="/contacts">
             Contacts
           </b-nav-item>
         </b-navbar-nav>
