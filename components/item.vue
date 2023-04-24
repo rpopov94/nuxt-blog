a73dca26e452ccf170bd3f3d4280d9c2530040cd
Roman Popov <i@romanpopoff.ru>
2023-04-24 11:31:12 +0300

    разделение логики

---
+++ components/item.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mb-2">
+    <b-card
+      :img-src="'/images/' + item.image"
+      img-alt="Image"
+      img-top
+      tag="article"
+      class="h-100"
+      style="min-height: 200px; max-width: 400px"
+    >
+      <b-card-text v-if="item && item?.link">
+        <b-link :href="item?.link" class="title">
+          {{ item.layout }}
+        </b-link>
+      </b-card-text>
+      <b-card-text v-else>
+        <b-link :to="item.path" class="title">
+          {{ item.layout }}
+        </b-link>
+      </b-card-text>
+      <b-card-text>
+        {{ item.abstract }}
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/items-cloud.vue
+++ components/items-cloud.vue
@@ -6,30 +6,7 @@
     <hr>
     <b-row>
       <b-col v-for="(item, id) in items" :key="id" md="4">
-        <div class="mb-2">
-          <b-card
-            :img-src="'/images/' + item.image"
-            img-alt="Image"
-            img-top
-            tag="article"
-            class="h-100"
-            style="min-height: 200px; max-width: 400px"
-          >
-            <b-card-text v-if="item && item?.link">
-              <b-link :href="item?.link" class="title">
-                {{ item.layout }}
-              </b-link>
-            </b-card-text>
-            <b-card-text v-else>
-              <b-link :to="item.path" class="title">
-                {{ item.layout }}
-              </b-link>
-            </b-card-text>
-            <b-card-text>
-              {{ item.abstract }}
-            </b-card-text>
-          </b-card>
-        </div>
+        <item :item="item"/>
       </b-col>
     </b-row>
   </div>
