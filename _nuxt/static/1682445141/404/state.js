c42f480c83c7815323019e0fee32ef94cd9304b8
rpopov94 <rpopov94@users.noreply.github.com>
2023-04-25 17:59:08 +0000

    deploy: 4e9f4dccd7792bbd5d60ed913a9106234254d91e

--- _nuxt/static/1682445141/404/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682445141",layout:"default",error:a,state:{articles:{article:a,articles:[]},projects:{project:a,projects:[]},store:{}},serverRendered:true,routePath:"\u002F404",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8144365"}}}}(null));
