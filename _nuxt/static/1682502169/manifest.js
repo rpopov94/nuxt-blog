ceca09eb38cb964af511e1ec8d06ed0e1ce30e8a
rpopov94 <rpopov94@users.noreply.github.com>
2023-04-26 09:42:54 +0000

    deploy: 9f07ba76049f5c35a8f598a4a78660216e010ce9

---
+++ _nuxt/static/1682502169/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002F404","\u002Farticles","\u002Farticles\u002Fpython_basics_part_1","\u002Farticles\u002Fpython_basics_part_2","\u002Farticles\u002Fbasics_methods"]})
