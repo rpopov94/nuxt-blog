5d4300a7825631db739815169117651f203dfa83
rpopov94 <rpopov94@users.noreply.github.com>
2023-06-11 11:17:46 +0000

    deploy: 02e44e159269c29b5b3fbc558bb61879fe80221a

---
+++ _nuxt/static/1686482259/articles/tags/oop/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686482259",layout:"default",error:a,state:{articles:{article:a,articles:[]},projects:{project:a,projects:[]},store:{}},serverRendered:true,routePath:"\u002Farticles\u002Ftags\u002Foop",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"28aa1783"}}}}(null));
